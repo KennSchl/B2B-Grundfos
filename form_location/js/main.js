a82fedb4f8dd2f095e8c3559ac9b920e4e418ef7
Asger <asger@daugberg.com>
2019-03-13 15:40:50 +0100

    Asger update 1.1
    Fonts, farver mm fra Design Guide

--- form_location/js/main.js
+++ form_location/js/main.js
@@ -6,41 +6,40 @@ console.log(window);
 console.log(navigator);
 
 navigator.geolocation.getCurrentPosition(position => {
-  console.log(position);
-  document.querySelector("#location").innerHTML += `
+    console.log(position);
+    document.querySelector("#location").innerHTML += `
   <p>Latitude: ${position.coords.latitude}</p>
   <p>Longitude: ${position.coords.longitude}</p>
   `;
 });
 
 fetch('http://ip-api.com/json').then(response => {
-  return response.json();
+    return response.json();
 }).then(location => {
-  console.log(location);
-  document.querySelector("#location").innerHTML += `
-  <p>City: ${location.city}</p>
-  <p>Country: ${location.country}</p>
+    console.log(location);
+    document.querySelector("#location").innerHTML += `
+  <p>${location.country}</p>
   `;
 });
 
-document.querySelector('#createUserBtn').addEventListener("click", function() {
-  createUser();
+document.querySelector('#createUserBtn').addEventListener("click", function () {
+    createUser();
 });
 
 function createUser() {
-  // declaring input field variables
-  let nameInput = document.querySelector('#name');
-  let emailInput = document.querySelector('#email');
+    // declaring input field variables
+    let nameInput = document.querySelector('#name');
+    let emailInput = document.querySelector('#email');
 
-  let newUser = {
-    name: nameInput.value,
-    email: emailInput.value
-  };
+    let newUser = {
+        name: nameInput.value,
+        email: emailInput.value
+    };
 
-  users.push(newUser);
-  console.log(users);
+    users.push(newUser);
+    console.log(users);
 
-  // reset input fields
-  nameInput.value = "";
-  emailInput.value = "";
+    // reset input fields
+    nameInput.value = "";
+    emailInput.value = "";
 }
