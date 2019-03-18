7f4168772188b0649e344a4171eafbc71fcf4ccf
daugberg <asger@daugberg.com>
2019-03-18 12:52:09 +0100

    Page 1 virker (næsten)

--- form_location/js/main.js
+++ form_location/js/main.js
@@ -34,34 +34,13 @@ function createUser() {
     emailInput.value = "";
 }
 
-// PASSWORD
+//PASSWORD
 
-// ----- Setup: add dummy password text field and add toggle button
-$('input[type=password]').each(function(){
-  var el = $(this), elPH = el.attr("placeholder");
-  
-  el.addClass("offPage").before('<input type="text" class="passText" placeholder="' + elPH + '" />');
-});
-
-$('form').append('<small><a href="#" class="togglePassText">Toggle Password Visibility</a></small>');
-
-
-// ----- keep text in sync
-$('input[type=password]').keyup(function(){
-  var elText = $(this).val();
-  $('.passText').val(elText);
-});
-$('.passText').keyup(function(){
-  var elText = $(this).val();
-  $('input[type=password]').val(elText);
-});
-
-// ----- Toggle button functions
-$('a.togglePassText').click(function(e){
-
-  $('input[type=password], .passText').toggleClass("offPage");
-
-  // -- prevent any default actions
-  e.preventDefault();
-
-});
+function myFunction() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
